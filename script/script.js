618ad869532fafd0eb5bb04c15fb3b00befa577d
yuuki-1337 <kaueyuukiyamamoto@gmail.com>
2025-03-22 00:10:54 -0300

    Introducao ao JavaScript

---
+++ script/script.js
@@ -0,0 +1,11 @@
+console.log("Hello world");
+
+let userEmail;
+let userPassword;
+
+document.getElementById("myLogin").onclick = function(){
+    userEmail = document.getElementById("myEmail").value;
+    userPassword = document.getElementById("myPassword").value;
+    console.log("Email: ",userEmail);
+    console.log("Password: ",userPassword);
+}
