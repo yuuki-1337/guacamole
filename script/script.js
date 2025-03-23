da45a02fecb035b6f6500b3f4562afc7260e81b7
yuuki-1337 <kaueyuukiyamamoto@gmail.com>
2025-03-22 23:50:17 -0300

    Home page

--- script/script.js
+++ script/script.js
@@ -3,12 +3,17 @@ console.log("Hello world");
 let userEmail;
 let userPassword;
 
-document.getElementById("myLogin").onclick = function(){
+document.getElementById("myLogin").onclick =  function(){
     userEmail = document.getElementById("myEmail").value;
     userPassword = document.getElementById("myPassword").value;
     console.log("Email: ",userEmail);
     console.log("Password: ",userPassword);
-    if(userEmail){
-        console.log("Erro")
-    }
+}
+
+function guacaPro(){
+    document.getElementById("textPrice").innerText = "Cursos a partir de 49,99 e muito mais"
+}
+
+function guacaBasic(){
+    document.getElementById("textPrice").innerText = "Todos os cursos a partir de R$ 39,99"
 }
